e87dd6222fee79ee4326eee3bd55c12cd6ce4876
Eric Meyer <emeyer@8thlight.com>
2016-12-15 13:07:36 -0600

    Include DashSpinner in Webpack bundle
     * Also include sample app

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,14 @@ const PROJECT_ROOT = path.resolve(__dirname)
 
 module.exports = {
   entry: [
+    "./bower_components/jquery/dist/jquery.js",
     "./bower_components/underscore/underscore.js",
-    "./scripts/dash-spinner/bar.js",
-    "./scripts/dash-spinner/foo.js",
-    "./scripts/dash-spinner/foo.coffee"
+    "./bower_components/backbone/backbone.js",
+    "./scripts/namespace.js",
+    "./scripts/dash-spinner/configuration.coffee",
+    "./scripts/dash-spinner/spinner.coffee",
+    "./scripts/sample_app/spinner_controls_view.coffee",
+    "./scripts/sample_app/main.coffee"
   ],
   output: {
     path: "./dist",
@@ -34,6 +38,9 @@ module.exports = {
     ]
   },
   plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery"
+    }),
     new webpack.ResolverPlugin(
       new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin(".bower.json", ["main"])
     )
@@ -41,6 +48,10 @@ module.exports = {
   resolve: {
     alias: {
       "dash_spinner": path.join(PROJECT_ROOT, "scripts", "dash-spinner")
-    }
+    },
+    modulesDirectories: [
+      path.join(PROJECT_ROOT, "node_modules"),
+      path.join(PROJECT_ROOT, "bower_components")
+    ]
   }
 }

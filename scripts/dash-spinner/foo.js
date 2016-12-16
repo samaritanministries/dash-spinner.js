0bd501bb6398d428e099d6b21a3d51330fb6e026
Eric Meyer <emeyer@8thlight.com>
2016-12-16 07:53:30 -0600

    Remove temporary files

--- scripts/dash-spinner/foo.js
+++
@@ -1,7 +0,0 @@
-console.log("foo.js")
-
-var doubled = _.map([1, 2, 3], function(number) {
-  return number * 2;
-})
-
-console.log(doubled);
--- spec/dash-spinner/foo_spec.coffee
+++
@@ -1,5 +0,0 @@
-import Baz from "dash_spinner/baz.js"
-
-describe "Foo", ->
-  it "imports Baz", ->
-    expect(Baz).toEqual(baz: "123")
